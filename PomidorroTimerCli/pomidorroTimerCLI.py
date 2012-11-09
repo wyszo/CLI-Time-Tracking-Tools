91f535003eaaaa2ea8b696f43f0b5d646573427f
Tomek Wyszomirski <tomek.wyszomirski@gmail.com>
2012-11-09 23:07:41 +0100

    Pomidorro Timer CLI v1.01

--- PomidorroTimerCli/pomidorroTimerCLI.py
+++ PomidorroTimerCli/pomidorroTimerCLI.py
@@ -1,26 +1,48 @@
 
-import os, sys
-import time
+import os, sys, subprocess, time, argparse
 
+#
+# A very simple cli utility for 'Pomidorro technique' (google it up)
+# (I don't know Python well, this script may contain silly code)
+#
+
+version = 1.01
+pomidorroLen = 25 # minutes
+breakLen = 5 # minutes
+
+# -------------------------
+
+def cmdExists(cmd):
+	return subprocess.call(["command", "-v", cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 def say(text):
-	cmd = "echo \"" + str(text) + "\" | espeak -a200 2> /dev/null" # -a: amplitude (volume): <0, 200>
-	os.system(cmd)
+	cmd = ""
 	
+	if cmdExists("espeak"):
+		cmd = "echo \"" + str(text) + "\" | espeak -a200 2> /dev/null" # -a: amplitude (volume): <0, 200>
+	else:
+		if cmdExists("say"):
+			cmd = "echo \"" + str(text) + "\" | say 2> /dev/null" 
+	
+	if cmd:
+		os.system(cmd)
+
+# -------------------------
 
 def waitNrOfMin(minutes):
 	secsInMinute = 60
 	sec = minutes * secsInMinute
 	i = 0
 	while i < minutes:
-			currMin = i
-			minLeft = minutes - currMin
+		currMin = i
+		minLeft = minutes - currMin
 
-			print "... 	[" + str(currMin) + " min, " + str(minLeft) + " left]"
+		print "... 	[" + str(currMin) + " min, " + str(minLeft) + " left]"
 
-			time.sleep(secsInMinute)
-			i = i+1
+		time.sleep(secsInMinute)
+		i = i+1
 
+# -------------------------
 
 def currTime():
 	currHour = time.strftime('%H')
@@ -28,10 +50,10 @@ def currTime():
 	currTime = currHour + ":" + currMin
 	return currTime
 
+# -------------------------
 
 def runPomidorroNr(pomidorroNr):
-	pomidorroLen = 25 # minutes
-	breakLen = 5 # minutes
+	global pomidorroLen
 	
 	helloStr = "Pomidorro #" + str(pomidorroNr) + ": Start [ current time: " + currTime() + " ]" 
 	hr = "" + "".join(['-' for i in range(0, len(helloStr))])
@@ -46,7 +68,10 @@ def runPomidorroNr(pomidorroNr):
 	print timesUp + " [time: " + str(currTime()) + "]"
 	say(timesUp)
 
-	# pormidorro - break
+
+def runBreak():
+	global breakLen
+	
 	waitNrOfMin(breakLen)
 
 	back2work = "Back to work"
@@ -54,42 +79,68 @@ def runPomidorroNr(pomidorroNr):
 	say(back2work)
 	raw_input("\nhit [Enter] to start next session")
 
-	return
+# -------------------------
 
-
-def pauseOrAbort():
+def pauseOrAbort(message, enterPressedMessage):
 	bQuit = True
 	try:
-		temp = raw_input('\n\nPress [enter] to restart current session or any other key to quit...\n')
+		temp = raw_input('\n\n' + message + '\n')
 
 		if temp == '':
-			print 'Restarting last session...\n'
+			print (enterPressedMessage + '\n')
 			bQuit = False
 		else:
-			print '\n(exit)'
+			print 
 	except KeyboardInterrupt:
-		print '\n(exit)'
+		print 
 	return bQuit
 
+# -------------------------
 
 def startSessions(startingNr):
 	nr = startingNr
 	bQuit = False
-	try:
-		while 1:
+	
+	while 1:
+		try:
 			runPomidorroNr(nr)
 			nr = nr+1
-	except KeyboardInterrupt:
-		bQuit = pauseOrAbort()
+			
+			try:
+				runBreak()
+			except KeyboardInterrupt:
+				bQuit = pauseOrAbort("Press [enter] to end break and start next session or any other key to quit...", "Starting next session...")
+				if bQuit:
+					break
+		except KeyboardInterrupt:
+			bQuit = pauseOrAbort("Press [enter] to restart current session or any other key to quit...", "Restarting last session...")
+			break
+					
 	return nr, bQuit
 	
+# -------------------------
+
+def startNrFromCliArgs():
+	descStr = "A very simple CLI utility for 'Pomidorro technique' (google it up)"
+	parser = argparse.ArgumentParser(description = descStr)
+	
+	parser.add_argument('--start_from', metavar='StartFromNr', default=1, type=int, help='Start counting pomidorro sessions from this number (defaults to 1)')
+	
+	#parse args
+	args = parser.parse_args()
+	startNr = args.start_from
+	return startNr
+
+# -------------------------
 
 def main():
-	nr = 1
+	startNr = startNrFromCliArgs()
+	print '\nPomidorro Timer CLI Utility v' + str(version)
+	
 	bQuit = False
 	while not bQuit:
-		result = startSessions(nr)
-		nr = result[0]
+		result = startSessions(startNr)
+		startNr = result[0]
 		bQuit = result[1]
 			
 	return
