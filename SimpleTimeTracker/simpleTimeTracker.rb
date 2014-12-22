a1404001f9b79ded4d320ea1780bb337cb503649
Tomasz Wyszomirski <tomasz@wyszomirski.co.uk>
2014-12-22 21:22:39 +0100

    Printing intro and getting current task added

--- SimpleTimeTracker/simpleTimeTracker.rb
+++ SimpleTimeTracker/simpleTimeTracker.rb
@@ -7,27 +7,74 @@ require 'optparse'
 require 'pp'
 
 
-version = 1.05
+$VERSION = 1.05
 
 
-def printVersion(version)
-	print "hello!\n"
-	print "version " + String(version)
+def parseLaunchArguments()
+	arguments = {} 
+
+	OptionParser.new do |opts|
+		descStr =  "A very simple time tracker. Shows time spend on a single activity.\n"
+		scriptFilename = File.basename(__FILE__)
+		usageStr = "Usage: " + scriptFilename + " [options]\n"
+
+		opts.banner = descStr + usageStr
+
+		opts.on("-h", "--help", "Displays help") do
+			puts opts
+			exit
+		end
+
+		opts.on("-t", "--time TIME", "Start Time (in minutes or format HH:MM, eg: \'3:23\' or format with \'h\' and \'m\' characters, like \'3h15m\').") do |time|
+			pp time
+			arguments[:time] = time
+		end
+
+		opts.on("-a", "--task TASK", 'Name of the activity which you want to track') do |task|
+			pp task
+			arguments[:task] = task
+		end
+	end.parse!
+
+	return arguments
 end
 
 
-OptionParser.new do |opts|
-	descStr =  "A very simple time tracker. Shows time spend on a single activity.\n"
-	scriptFilename = File.basename(__FILE__)
-	usageStr = "Usage: " + scriptFilename + " [options]\n"
+def printIntro(version)
+	puts ""
+	puts "-----------------------------------------"
+    puts "A very simple Time Tracking Utility v" + String(version) 
+    puts "-----------------------------------------"
+    puts ""
+end
 
-	opts.banner = descStr + usageStr
 
-	opts.on("-h", "--help", "Displays help") do
-		puts opts
-		exit
+def grabTaskName(currentTaskName)
+	taskName = currentTaskName
+
+	if String(currentTaskName).length == 0
+		taskName = ''
+		begin 
+			puts "Enter task name: "
+			rawTaskName = gets
+			rawTaskName ||= ''
+			taskName = rawTaskName.chomp!
+		rescue Interrupt
+			exit
+		end
 	end
-end.parse!
+	return taskName
+end
 
 
-printVersion(version)
+def main()
+	launchArguments = parseLaunchArguments()
+	printIntro($VERSION)
+	launchArguments[:task] = grabTaskName(launchArguments[:task])
+end
+
+
+main()
+
+
+# 2. enter task name (if not present)
